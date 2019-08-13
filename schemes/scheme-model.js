af3bf56b01bbdae6b43f072a9ae204310e23ee31
Ihab Ali <ihab@live.com>
2019-08-13 02:27:10 -0500

    added 'findSteps' helper method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,8 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   find,
-  findById
-  // findSteps,
+  findById,
+  findSteps
   // add,
   // update,
   // remove
@@ -16,4 +16,13 @@ function find() {
 function findById(id) {
   return db("schemes").where({ id });
   //   .first(); // if not destructured
+}
+
+function findSteps(scheme_id) {
+  return db("steps as st")
+    .join("schemes as sc", "st.scheme_id", "=", "sc.id")
+    .select("st.step_number", "st.instructions")
+    .orderBy("st.step_number", "asc")
+    .groupBy("st.step_number")
+    .where({ scheme_id });
 }
