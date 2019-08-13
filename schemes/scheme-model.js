95ed0d5d5cd89bf77034a35b7dbae8cc0cafea3a
Ihab Ali <ihab@live.com>
2019-08-13 02:24:49 -0500

    added 'find' helper method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,14 @@
 const db = require("../data/dbConfig");
+
+module.exports = {
+  find
+  // findById,
+  // findSteps,
+  // add,
+  // update,
+  // remove
+};
+
+function find() {
+  return db("schemes");
+}
